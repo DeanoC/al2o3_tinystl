2c1b512c68074131223bd409dc3856038d3174e7
Dean Calver <me@deanoc.com>
2019-05-17 13:03:20 +0300

    OS X now working :) Added central SET_MIN_VERSION macro

--- include/al2o3_tinystl/string.hpp
+++ include/al2o3_tinystl/string.hpp
@@ -721,7 +721,7 @@ static inline size_t hash(const basic_string<allocator>& value) {
 
 typedef basic_string<TINYSTL_ALLOCATOR> string;
 
-#if PLATFORM == PLATFORM_WINDOWS
+#if AL2O3_PLATFORM == AL2O3_PLATFORM_WINDOWS
 // basic to_string support, probably slow etc.
 #define TO_STRING_GEN(TYPE, FMT) \
 static inline string to_string(TYPE const t) { \
@@ -749,7 +749,7 @@ TO_STRING_GEN(int32_t, "%" PRIi32)
 TO_STRING_GEN(int64_t, "%" PRIi64)
 TO_STRING_GEN(float, "%f")
 TO_STRING_GEN(double, "%f")
-#if PLATFORM != PLATFORM_WINDOWS
+#if AL2O3_PLATFORM != AL2O3_PLATFORM_WINDOWS
 TO_STRING_GEN(size_t, "%zu")
 #endif
 
